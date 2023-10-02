36c9e622f8446be5e28a151ce3b581a327da70bf
Rubens Lyra <rubensvlyra@gmail.com>
2023-10-02 04:02:45 -0300

    diagrama

--- EstimationApp/Models/Estimate.cs
+++
@@ -1,9 +0,0 @@
-public class Estimate
-{
-    public int Id { get; set; }
-    public string? ProjectName { get; set; }
-    public string? Description { get; set; }
-    public double BestCase { get; set; }
-    public double MostLikelyCase { get; set; }
-    public double WorstCase { get; set; }
-}
